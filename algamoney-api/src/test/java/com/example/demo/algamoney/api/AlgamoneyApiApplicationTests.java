00e5a5d79d41e6ed4aa4562afe17e4c2a21dc8aa
Ricardo <Ricardo@DESKTOP-VJMSBBA>
2021-02-24 22:12:32 -0300

    3.6 OK

---
+++ algamoney-api/src/test/java/com/example/demo/algamoney/api/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo.algamoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
